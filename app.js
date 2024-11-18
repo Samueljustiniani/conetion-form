4546ce8953f10706a8020b7ea15e789b79c77854
Samuel Imanol <167726711+Samueljustiniani@users.noreply.github.com>
2024-11-18 17:00:29 -0500

    nodejs12

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const path = require('path');
+const app = express();
+const ip = '54.167.194.202';
+const port = 3000;
+
+// Configuración de middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Configuración de conexión a MySQL con un pool
+let pool = mysql.createPool({
+    host: "practica.c920g66kq4od.us-east-1.rds.amazonaws.com",
+    database: "banco_db",
+    user: "admin",
+    password: "samuel123",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Manejo de la solicitud POST del formulario
+app.post('/submit-form', (req, res) => {
+    const { numeroCuenta, nombreCliente, saldo, tipoCuenta } = req.body;
+
+    const query = 'INSERT INTO Clientes (numeroCuenta, nombreCliente, saldo, tipoCuenta) VALUES (?, ?, ?, ?)';
+    pool.query(query, [numeroCuenta, nombreCliente, saldo, tipoCuenta], (err, result) => {
+        if (err) {
+            console.error('Error al insertar datos: ' + err.stack);
+            return res.status(500).send('Ocurrió un error al procesar tu consulta.');
+        }
+        // Enviar una respuesta de éxito
+        res.status(200).send('Cliente registrado exitosamente');
+    });
+});
+
+// Página principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'eso.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://${ip}:${port}`);
+});
